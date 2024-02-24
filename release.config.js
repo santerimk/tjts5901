ab2119cd53a84868cb64b9a48830d3d1517f5825
Juho Enäkoski <juhe-keksi@hotmail.com>
2024-02-24 21:17:51 +0200

    feat: added codecov coverage

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config = {
-	branches: ["main", { name: "test", prerelease: false }],
+	branches: ["main"],
 	plugins: [
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
