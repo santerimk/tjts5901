eb9c5f2707e9b0b6a046b450d12cc0cd726e57c3
juolenak <juolenak@student.jyu.fi>
2024-03-05 21:10:27 +0200

    feat: Added BDD workflow pipeline

--- features/steps/register_steps.py
+++ features/steps/register_steps.py
@@ -7,18 +7,6 @@ def given_user_on_registration_page(context):
     assert context.response.status_code == 200, "Couldn't reach registry page."
 
 
-@when('the user submits a registration form with an existing username')
-def when_user_submits_form_with_existing_username(context):
-    form_data = {
-        'first_name': 'Alex',
-        'last_name': 'Tandori',
-        'tradername': 'alex_t',
-        'password': 'Pass123%'
-    }
-    context.response = context.client.post('/register', data=form_data, follow_redirects=True)
-    assert context.response.status_code == 200, "Couldn't reach register."
-
-
 @when('the user submits a registration form with valid data')
 def when_user_submits_form_with_valid_data(context):
     form_data = {
@@ -29,6 +17,18 @@ def when_user_submits_form_with_valid_data(context):
     }
     context.response = context.client.post('/register', data=form_data, follow_redirects=True)
     assert context.response.status_code == 200, "Couldn't complete registration with valid data."
+
+
+@when('the user submits a registration form with an existing username')
+def when_user_submits_form_with_existing_username(context):
+    form_data = {
+        'first_name': 'Alex',
+        'last_name': 'Tandori',
+        'tradername': 'alex_t',
+        'password': 'Pass123%'
+    }
+    context.response = context.client.post('/register', data=form_data, follow_redirects=True)
+    assert context.response.status_code == 200, "Couldn't reach register."
 
 
 @when('the user submits a registration form with invalid data')
@@ -50,9 +50,9 @@ def then_user_registered_successfully(context):
 
 @then('the user should see an error message about username uniqueness')
 def then_user_sees_username_uniqueness_error(context):
-    assert 'Please choose a different one.' in context.response.data.decode(), "Tradername error message wasn't displayed."
+    assert 'Please choose a different one.' in context.response.data.decode(), context.response.data #"Tradername error message wasn't displayed."
 
 
 @then('the user should see an error message about invalid data')
 def then_user_sees_invalid_data_error(context):
-    assert 'First name is required.' in context.response.data.decode(), context.response.data #"First name error message wasn't displayed."
+    assert 'First name is required.' in context.response.data.decode(), "First name error message wasn't displayed."
