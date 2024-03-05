c5b333f7753ef6a1361413403b679317c442e785
juolenak <juolenak@student.jyu.fi>
2024-03-05 20:45:14 +0200

    feat: Added BDD workflow pipeline

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,18 @@
 from main import app
 from stockmarket import reset_and_populate
+from flask_wtf.csrf import CSRFProtect
+import os
+
+
+csrf = None  # Initialize CSRFProtect as None initially
 
 
 def before_scenario(context, scenario):
     context.client = app.test_client()
     context.client.testing = True
+    global csrf
+    if not context.client.testing:  # Only enable CSRF protection if not in testing mode
+        csrf = CSRFProtect(app)
 
 
 def after_scenario(context, scenario):
