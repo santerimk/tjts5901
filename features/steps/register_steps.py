1f6d2a59a0d4328bf953ddf14548ef6ee35adef6
juolenak <juolenak@student.jyu.fi>
2024-03-05 21:29:34 +0200

    feat: Added BDD workflow pipeline

--- features/steps/register_steps.py
+++ features/steps/register_steps.py
@@ -48,7 +48,7 @@ def when_user_submits_form_with_invalid_data(context):
 
 @then('the user should be registered successfully')
 def then_user_registered_successfully(context):
-    assert 'Login' in context.response.data.decode(), context.response.data #"User was not registered successfully."
+    assert 'Login' in context.response.data.decode(), "User was not registered successfully."
 
 
 @then('the user should see an error message about username uniqueness')
